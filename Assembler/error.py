13b9806552385b61032fa8b951546b658ebee431
arnav-makkar <142909930+arnav-makkar@users.noreply.github.com>
2024-03-11 22:51:30 +0530

    Error file completed

--- Assembler/error.py
+++ Assembler/error.py
@@ -1,17 +1,18 @@
-import assembler_code as ps
+import assembler_code as ac
 
 def suggestions_for(instruction):
-    all_instructions = ps.R_Type_Instructions + ps.I_Type_Instructions + ps.S_Type_Instructions + ps.B_Type_Instructions + ps.U_Type_Instructions + ps.J_Type_Instructions + ps.Bonus
 
-    def suggester(instruction):
-        def count_matches(i):
-            return sum(1 for x in instruction if x in i)
-        return count_matches
+    all_instructions = ac.all_instructions
 
-    suggester_function = suggester(instruction)
-    sorted_instructions = sorted(all_instructions, key=suggester_function, reverse=True)
-    suggested_instructions = [i for i in sorted_instructions if suggester_function(i) > len(i) - 2]
+    def count(i):
+        cnt = 0
 
-    print(f"{instruction} did not match any known instruction, perhaps you meant") 
-    print(*suggested_instructions, sep=", ")
+        for x in instruction:
+            if(x in i):
+                cnt += 1
+        return cnt
 
+    relevant_instructions = filter(lambda x: count(x)>len(x)-1, all_instructions)
+
+    print(f"'{instruction}' did not match any known instructions. Did you mean any of these? ") 
+    print(*relevant_instructions, sep=", ")
