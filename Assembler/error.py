98f36bbcc0a6bb53af9b40c5f53d7e7bae80e837
anirudhsgh7 <156465517+anirudhsgh7@users.noreply.github.com>
2024-03-11 21:57:47 +0530

    Update error.py

--- Assembler/error.py
+++ Assembler/error.py
@@ -1,22 +1,17 @@
 import assembler_code as ps
 
 def suggestions_for(instruction):
-    all_instructions = [
-        *ps.R_Type_Instructions,
-        *ps.I_Type_Instructions,
-        *ps.S_Type_Instructions,
-        *ps.B_Type_Instructions,
-        *ps.U_Type_Instructions,
-        *ps.J_Type_Instructions,
-        *ps.Bonus
-    ]
+    all_instructions = ps.R_Type_Instructions + ps.I_Type_Instructions + ps.S_Type_Instructions + ps.B_Type_Instructions + ps.U_Type_Instructions + ps.J_Type_Instructions + ps.Bonus
 
-    matches = [(i, sum(1 for x in instruction if x in i)) for i in all_instructions]
-    matches.sort(key=lambda x: x[1], reverse=True)
+    def suggester(instruction):
+        def count_matches(i):
+            return sum(1 for x in instruction if x in i)
+        return count_matches
 
-    suggested_instructions = [ins for ins, count in matches if count > len(ins) - 2]
+    suggester_function = suggester(instruction)
+    sorted_instructions = sorted(all_instructions, key=suggester_function, reverse=True)
+    suggested_instructions = [i for i in sorted_instructions if suggester_function(i) > len(i) - 2]
 
-    print(f"{instruction} did not match any known instruction, perhaps you meant:") 
+    print(f"{instruction} did not match any known instruction, perhaps you meant") 
     print(*suggested_instructions, sep=", ")
 
-suggestions_for(instruction)
